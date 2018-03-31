a97a1ced259c27bf0012ef505e8a2b276cb97b0e
Matthias Van Limbergen <37349178+MVanLimbergen@users.noreply.github.com>
2018-03-31 13:23:47 +0200

    Update Blissindependence.R

--- R/Blissindependence.R
+++ R/Blissindependence.R
@@ -14,19 +14,22 @@ Blissindependence <- function(doseInput, parmInput, ...){
   
   pars <- parmInput
   
-  #We need the value between 0 and 1 for Bliss not estimated effect
+  #We need the percentage value between 0 and 1 for Bliss estimated effect
   Hilleq <- function(dose,b,logEC50){
     Hilleqread <- 1/(1 + (exp(logEC50)/dose)^(abs(b)))
   }
   
   #Bliss independence combination
-  applyfunction <- function(pred1, pred2){
+  applyfunction <- function(pred1, pred2,L,U1,U2){
+    #prediction value in percentage
     predcombo <- pred1+pred2-pred1*pred2
+    #scale back to readout value by multiplying with the highest maximum response
+    predcombo <- ifelse(U1 > U2 ,L+(U1-L)*predcombo ,L+(U2-L)*predcombo)
     return(predcombo)
     }
   
   pred1 <- Hilleq(doseInput[, "d1"], pars["h1"], pars["e1"])
   pred2 <- Hilleq(doseInput[, "d2"], pars["h2"], pars["e2"])
   
-  apply(cbind(pred1,pred2),1, applyfunction)
+  apply(cbind(pred1,pred2,pars["b"], pars["m1"],pars["m2"]),1, applyfunction)
 }
