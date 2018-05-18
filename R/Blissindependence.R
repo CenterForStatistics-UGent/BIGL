2233d5797ae12efd9d88e02d0cb194dbb791b74e
Matthias Van Limbergen <37349178+MVanLimbergen@users.noreply.github.com>
2018-05-18 17:23:58 +0200

    Update Blissindependence.R

--- R/Blissindependence.R
+++ R/Blissindependence.R
@@ -10,6 +10,18 @@
 #'   \code{MarginalFit} object.
 #' @param ... Further arguments that are currently unused
 
+#' Bliss Independence Model
+#' 
+#' This function returns fractional response levels for when these are based on
+#' Bliss Independence Model.
+#' 
+#' @param doseInput Dose-response dataframe containing \code{"d1"} and
+#'   \code{"d2"} columns
+#' @param parmInput Numeric vector or list with appropriately named
+#'   parameter inputs. Typically, it will be coefficients from a
+#'   \code{MarginalFit} object.
+#' @param ... Further arguments that are currently unused
+
 Blissindependence <- function(doseInput, parmInput, ...){
   
   pars <- parmInput
@@ -19,15 +31,20 @@ Blissindependence <- function(doseInput, parmInput, ...){
     Hilleqread <- 1/(1 + (exp(logEC50)/dose)^(abs(b)))
   }
   
+  #calculate and rescale to max upper
   pred1 <- Hilleq(doseInput[, "d1"], pars["h1"], pars["e1"])
+  pred1 <- pred1*abs(pars["m1"]-pars["b"])/max(c(abs(pars["m1"]-pars["b"]),abs(pars["m2"]-pars["b"])))
   pred2 <- Hilleq(doseInput[, "d2"], pars["h2"], pars["e2"])
-  
+  pred2 <- pred2*abs(pars["m2"]-pars["b"])/max(c(abs(pars["m1"]-pars["b"]),abs(pars["m2"]-pars["b"])))
   #Bliss independence combination
   applyfunction <- function(param){
     #prediction value in percentage
     predcombo <- param[1]+param[2]-param[1]*param[2]
-    #scale back to readout value by multiplying with the highest maximum response
-    predcombo <- ifelse(param[4] > param[5] ,param[3]+(param[4]-param[3])*predcombo ,param[3]+(param[5]-param[3])*predcombo)
+    rise = 1
+    if(param[3]>param[4] & param[3] > param[5]){
+      rise = -1
+    }
+    predcombo <- param[3]+rise*max(c(abs(param[4]-param[3]),abs(param[5]-param[3])))*predcombo
     return(predcombo)
     }
   
